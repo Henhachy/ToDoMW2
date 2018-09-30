e752287688aba043fb2e020bfeaaa0ddc25f0dff
Henhachy <michal.turbo1@gmail.com>
2018-09-30 23:07:32 +0200

    Add methods service Table

--- ToDoMW/TableViewController.swift
+++ ToDoMW/TableViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    let itemArray = ["Tomek", "Marek", "Franek"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+        
+    }
+    
+    
+    
+    
 
 }
 
