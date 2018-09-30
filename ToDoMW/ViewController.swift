e65adce4e911cc73ef1191b5fba31d4d17017be6
Henhachy <michal.turbo1@gmail.com>
2018-09-30 19:21:13 +0200

    Initial Commit

---
+++ ToDoMW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoMW
+//
+//  Created by Michał Woś on 30/09/2018.
+//  Copyright © 2018 Michał Woś. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
